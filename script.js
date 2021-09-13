808b73493c4b3185841f49bef78543a165368108
Eugene Kang <kangeuge90@gmail.com>
2021-09-13 18:11:43 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Create a form with two inputs: a number input (or range input) for count 
+// and a select input for the type of coin: Penny, Nickel, Dime, or Quarter.
+"use strict"; {
+    function coinMaker
+
+    const form = document.getElementById('theForm');
+    const data = new FormData(form);
+    let coinNumber = data.get('coinNumber');
+    let coinType = data.get('coinValue');
+
+    button.addEventListener('submit', e => {
+        e.preventDefault();
+    })
+}
